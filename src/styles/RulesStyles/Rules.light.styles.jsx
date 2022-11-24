6bbc53c3b5a3b7826c12c53cdb81d6bc122ed928
Peter Calabrese <peter@calabrese.tech>
2022-11-24 10:09:49 -0500

    rules modal light mode

--- src/styles/RulesStyles/Rules.light.styles.jsx
+++ src/styles/RulesStyles/Rules.light.styles.jsx
@@ -5,6 +5,35 @@ export default StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     height: '100%',
-    backgroundColor: '#fff',
+    backgroundColor: '#eceff4',
+  },
+  header: {
+    color: '#4C566A',
+    fontSize: 35,
+    fontWeight: '700',
+    top: 50,
+    position: 'absolute',
+  },
+  description: {
+    marginTop: -20,
+    color: '#4C566A',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: '500',
+  },
+  cardTypeRule: {
+    marginTop: 30,
+    color: '#4C566A',
+    fontSize: 16,
+    fontWeight: '500',
+    textAlign: 'left',
+  },
+  backButton: {
+    position: 'absolute',
+    bottom: 60,
+  },
+  backButtonText: {
+    color: '#4C566A',
+    fontSize: 16,
   },
 });
