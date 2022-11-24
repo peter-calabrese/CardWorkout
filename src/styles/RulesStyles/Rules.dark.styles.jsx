0b0c74674b93a94bd4a86022ac552584111ada16
Peter Calabrese <peter@calabrese.tech>
2022-11-23 23:56:09 -0500

    dark and light mode styles

---
+++ src/styles/RulesStyles/Rules.dark.styles.jsx
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    backgroundColor: '#121212',
+  },
+});
