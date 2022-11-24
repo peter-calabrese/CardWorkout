5462add7854731ae52c2b57674c5761cfa22487c
Peter Calabrese <peter@calabrese.tech>
2022-11-24 10:07:41 -0500

    rules modal nord theme dark

--- src/styles/RulesStyles/Rules.dark.styles.jsx
+++ src/styles/RulesStyles/Rules.dark.styles.jsx
@@ -5,6 +5,35 @@ export default StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     height: '100%',
-    backgroundColor: '#121212',
+    backgroundColor: '#2e3440',
+  },
+  header: {
+    color: '#e5e9f0',
+    fontSize: 35,
+    fontWeight: '700',
+    top: 50,
+    position: 'absolute',
+  },
+  description: {
+    marginTop: -20,
+    color: '#e5e9f0',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: '500',
+  },
+  cardTypeRule: {
+    marginTop: 30,
+    color: '#e5e9f0',
+    fontSize: 16,
+    fontWeight: '500',
+    textAlign: 'left',
+  },
+  backButton: {
+    position: 'absolute',
+    bottom: 60,
+  },
+  backButtonText: {
+    color: '#e5e9f0',
+    fontSize: 16,
   },
 });
